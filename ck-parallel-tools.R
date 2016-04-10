1884eec5f135afefd773dc8dc1747f121024939a
Chris Kennedy <chrisken@gmail.com>
2016-04-09 22:05:24 -0700

    more tweaks

--- ck-parallel-tools.R
+++ ck-parallel-tools.R
@@ -2,17 +2,20 @@
 # By default it uses a multinode cluster if available, otherwise sets up multicore via doMC.
 # Libraries required: parallel, doParallel, doMC, RhpcBLASctl, foreach
 setup_parallelism = function(conf = NULL, type="either", allow_multinode = T,
-                             machine_list = Sys.getenv("SLURM_NODELIST"), outfile = "") {
+                             machine_list = Sys.getenv("SLURM_NODELIST"),
+                             cpus_per_node = as.numeric(Sys.getenv("SLURM_CPUS_ON_NODE")),
+                             outfile = "") {
   # Indicator for multi-node parallelism.
   multinode = F
 
-  # Check if we are on SLURM with multiple machine access.
+  # Check if we have multiple machine access.
   if (allow_multinode) {
     machines = strsplit(machine_list, ",")[[1]]
     if (length(machines) > 1) {
       cat("Have multi-node access for parallelism with", length(machines), "machines:", machines, "\n")
       # NOTE: this may be a bad config if the nodes have different core counts.
-      cores = rep(machines, each = as.numeric(Sys.getenv("SLURM_CPUS_ON_NODE")) )
+      cores = rep(machines, each = cpus_per_node)
+      cat("Multi-node cores enabled:", cores, "\n")
       multinode = T
     }
   }
@@ -31,7 +34,9 @@ setup_parallelism = function(conf = NULL, type="either", allow_multinode = T,
   if (multinode || type %in% c("cluster", "doParallel")) {
     # Outfile = "" allows output from within foreach to be displayed while in RStudio.
     # TODO: figure out how to suppress the output from makeCluster()
-    capture.output({ cl = parallel::makeCluster(cores, outfile = outfile) })
+    #capture.output({ cl = parallel::makeCluster(cores, outfile = outfile) })
+    cat("Starting multinode cluster with cores:", cores, "\n")
+    capture.output({ cl = parallel::makeCluster(cores, type="PSOCK", outfile = outfile) })
     # doParallel supports multicore and multinode parallelism, but may require
     # explicitly exporting functions and libraries across the cluster.
     registerDoParallel(cl)
